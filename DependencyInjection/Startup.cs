a20c6d1e2dc18b764d8bd97c8fae70ca6a4c3e97
Suvankar Das <hi.suvankar@gmail.com>
2024-09-14 13:35:12 +0530

    test

--- DependencyInjection/Startup.cs
+++ DependencyInjection/Startup.cs
@@ -1,6 +1,7 @@
 using DependencyInjection.Data;
 using DependencyInjection.Service;
 using DependencyInjection.Utility.AppSettingsClasses;
+using DependencyInjection.Utility.DIAppConfiguration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -43,11 +44,8 @@ namespace DependencyInjection
             // Register the service to IOC container
             services.AddTransient<IMarketForecaster, MarketForecaster>();
 
-            //GetSection() required a key and The key would be the exact same name that we have in appsettings.json.
-            services.Configure<WazeForecast>(Configuration.GetSection("WazeForecast"));
-            services.Configure<Stripe>(Configuration.GetSection("Stripe"));
-            services.Configure<Twilio>(Configuration.GetSection("Twilio"));
-            services.Configure<SendGrid>(Configuration.GetSection("SendGrid"));
+            
+            DI_AppConfigurationSettings.AddAppSettingsConfig(services, Configuration);
 
 
 
---
+++ DependencyInjection/Utility/DIAppConfiguration/DI_AppConfigurationSettings.cs
@@ -0,0 +1,21 @@
+﻿using DependencyInjection.Utility.AppSettingsClasses;
+using Humanizer.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjection.Utility.DIAppConfiguration
+{
+    public static class DI_AppConfigurationSettings
+    {
+        public static IServiceCollection AddAppSettingsConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            //GetSection() required a key and The key would be the exact same name that we have in appsettings.json.
+            services.Configure<WazeForecast>(configuration.GetSection("WazeForecast"));
+            services.Configure<Stripe>(configuration.GetSection("Stripe"));
+            services.Configure<Twilio>(configuration.GetSection("Twilio"));
+            services.Configure<SendGrid>(configuration.GetSection("SendGrid"));
+
+            return services;
+        }
+    }
+}
